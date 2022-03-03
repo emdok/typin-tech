020de0ce0185ff36b9b95ac5c0cceba69c935ac2
Emily Dokken <dokkenemily@gmail.com>
2022-03-02 21:36:11 -0800

    add comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,10 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-class Post extends Model {}
 
-class Post extends Model {};
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -12,15 +11,11 @@ Post.init(
       primaryKey: true,
       autoIncrement: true
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    post_url: {
+    comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isURL: true
+        len: [1]
       }
     },
     user_id: {
@@ -29,14 +24,21 @@ Post.init(
         model: 'user',
         key: 'id'
       }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
     }
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post'
+    modelName: 'comment'
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
