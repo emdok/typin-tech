891d18ab44478bcd9e34ed73a76306f1403700d3
Emily Dokken <dokkenemily@gmail.com>
2022-03-02 21:48:50 -0800

    add home-routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-// find all comments
 router.get('/', (req, res) => {
   Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
@@ -11,11 +10,10 @@ router.get('/', (req, res) => {
     });
 });
 
-// create new comment
 router.post('/', (req, res) => {
   Comment.create({
     comment_text: req.body.comment_text,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
     post_id: req.body.post_id
   })
     .then(dbCommentData => res.json(dbCommentData))
@@ -25,7 +23,6 @@ router.post('/', (req, res) => {
     });
 });
 
-// delete a comment
 router.delete('/:id', (req, res) => {
   Comment.destroy({
     where: {
