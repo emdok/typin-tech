f510c39dd5e6af7061d207fedea000f115ea05cc
Emily Dokken <dokkenemily@gmail.com>
2022-03-03 23:14:09 -0800

    updated event listener to use click and get element by id

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,14 +1,14 @@
+
 async function commentFormHandler(event) {
     event.preventDefault();
-
+    
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
 
     console.log(comment_text, post_id);
-    console.log('hello');
 
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.getElementById('comment-submit').addEventListener('click', commentFormHandler);
