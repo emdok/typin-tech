cf6cfb807ad66cd501cad7dfa2487744aa1f0333
Emily Dokken <dokkenemily@gmail.com>
2022-03-03 22:56:19 -0800

    update font family for site

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,12 +2,12 @@ async function commentFormHandler(event) {
     event.preventDefault();
 
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-    
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
 
     console.log(comment_text, post_id);
+    console.log('hello');
 
 }
 
