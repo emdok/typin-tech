adffe512a3cddc988bdf22e50cfb13bd7be063a2
Emily Dokken <dokkenemily@gmail.com>
2022-03-05 10:48:54 -0800

    add format plural helper and test

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,13 @@
+const { format_date, format_plural } = require('../utils/helpers');
+
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
   
     expect(format_date(date)).toBe('3/20/2020');
+  });
+
+  test('format_plural() returns a plural word when value greater than 1', () => {
+  
+    expect(format_plural("Tiger", 2)).toBe('Tigers');
+    expect(format_plural("Lion", 1)).toBe('Lion');
   });
