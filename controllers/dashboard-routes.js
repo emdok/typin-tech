6132bc73a3cd09aab70516b5dfd60654888f42f9
Emily Dokken <dokkenemily@gmail.com>
2022-03-05 15:58:59 -0800

    update edit post route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,10 +40,7 @@ router.get('/', withAuth, (req, res) => {
   });
 
   router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-      where: {
-        user_id: req.session.user_id
-      },
+    Post.findByPk(req.params.id, {
       attributes: [
         'id',
         'post_url',
@@ -66,11 +63,18 @@ router.get('/', withAuth, (req, res) => {
       ]
     })
       .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('/edit-post', { posts, loggedIn: true });
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
+          
+          res.render('edit-post', {
+            post,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
       })
       .catch(err => {
-        console.log(err);
         res.status(500).json(err);
       });
   });
