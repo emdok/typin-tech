a386c5a9ffe145a594207ff426239cf644871112
Emily Dokken <dokkenemily@gmail.com>
2022-03-05 17:33:06 -0800

    display signup if signup link clicked, else show login

---
+++ controllers/signup-route.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// '/signup'
+router.get('/', (req, res) => {
+    const signup = true;
+    res.render('login', { signup });
+})
+
+module.exports = router;
