655d2cc3407441608a97816608306fe8ed96e84a
Emily Dokken <dokkenemily@gmail.com>
2022-03-06 14:39:30 -0800

    add functionality for post text in add post js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -3,6 +3,8 @@ async function newFormHandler(event) {
   
     const title = document.querySelector('input[name="post-title"]').value;
     const post_url = document.querySelector('input[name="post-url"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
